39ef1eaae2d836242b01511e4c7e57ff9d5c45c0
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2025-01-03 00:33:29 +0100

    updated app admin

--- Controller/Admin/AdminDashboard.js
+++ Controller/Admin/AdminDashboard.js
@@ -1,99 +1,43 @@
 const db = require('../../Model/dbConfig')
 const asyncHandler = require("express-async-handler");
 const bcrypt = require('bcryptjs');
-const { sendEmail } =require("../email/email")
 const currentDate = require("../../util/Date/currentDate")
-var validator = require('validator');
 
-//create new user or signup
+
+//admin data route
 const AdminData = asyncHandler ( async (req,res)=>{
+
     const userID= req.body.userID
-    const SQL = `SELECT * from users where id=\'${userID}\'`
+    const SQL = `SELECT * from account where userID=\'${userID}\'`
+    let currentBalance = 0;
+    let availableBalance = 0;
+    let transactionHistory = 0;
   
     db.query(SQL,(err,result)=>{
         if(err) {
-          //return res.status(500).json({message: "Error Querying database"});
-        }else if((result!=undefined)&(result.length!=0)){
-            const user = result[0]
-            const {isAdmin} = user
-            if(isAdmin===1){
-                const SQL = `SELECT * FROM statistics`
-                db.query(SQL,(error,result)=>{
-                   if(error){
-                       console.error(error)
-                    //   return res.status(500).send({message: "Error Querying database"});
-                   }else{
-                       console.log(result)
-                       res.send(result[0])
-                   }
+            console.log(err)
+          return res.status(500).json({message: "Error Querying database"});
+        }
 
-
-                })
+          availableBalance = result[0].availableBalance
+          currentBalance = result[0].currentBalance
+          const SQL = `SELECT * from transactionHistory where userID=\'${userID}\'`
+          db.query(SQL,(err,result)=>{
+            if(err) {
+                console.log(err)
+              return res.status(500).json({message: "Error Querying database"});
             }
-        }})
+                transactionHistory = result.length
+                 console.log(transactionHistory)
+                  res.status(201).json({currentBalance:currentBalance,availableBalance:availableBalance,transactionHistory:transactionHistory})
+            })
+                
+      
+        })
             
      }) 
 
 
- const getAllVisitorsAdmin = asyncHandler ( async (req,res)=>{
-
-    
-        const SQL = `SELECT * from visitor  ORDER BY id DESC`;
-        
-    db.query(SQL,(err,result)=>{
-       if(err){
-           res.status(500).send(err);
-       }
-         
-        res.status(201).json({result:result})
-     
-    })
-    
-    })
-
-
-
-
-const deleteVisitor = asyncHandler ( async (req,res)=>{
-
-    const {userID,visitorID} = req.body
-    const SQL = `SELECT * from users  WHERE id=${userID}`;//check if its an admin user
-    
-db.query(SQL,(error,result)=>{
-   if(error){
-    res.status(401).json({status:0,message:"ERROR IN ACCESSING DATABASE"})
-   }else if((result!=undefined)&(result.length!=0)){
-      const user = result[0];
-      const {isAdmin} = user
-      console.log(isAdmin)
-      if(isAdmin===1){
-        const SQL = "SELECT * from statistics"
-        db.query(SQL,(error,result)=>{
-            if(error){
-                console.log(error)
-                return res.status(500).json({status:0,message: "Error Querying database"});
-            }else if((result!=undefined)&(result.length!=0)){
-                const visitorCounter = Number(result[0].visitorCounter)
-                const SQL = `UPDATE statistics SET visitorCounter=\'${(visitorCounter - 1)}\' WHERE id=1`
-                db.query(SQL,(error,result)=>{
-                if(error){
-                    console.log(error)
-                   return res.status(500).json({status:0,message: "Error Querying database"});
-                }else{
-                    
-                    const SQL = `DELETE from statistics WHERE id=${visitorID}`;
-                    db.query(SQL,(error,result)=>{
-                       if(error){
-                           res.status(201).json({status:0,message:"Error Querying database"})
-                       }else{
-                           res.status(201).json({status:1,message:"VISITOR RECORD DELETED SUCCESSFULLY"})
-                       }
-           
-                    })
-
-                  }
-             })
-         }})
 
 
 
@@ -101,28 +45,6 @@ db.query(SQL,(error,result)=>{
 
 
 
-
-
-
-
-      }else{
-        res.status(403).json({status:1,message:"ACCESS DENIED"})
-      }
-
-   }else{
-    res.status(201).json({status:0,message:"Unauthorized action!!"})
- 
-   }
-})
-})
-    
-
-
-
-
-
-
-
-  module.exports = {deleteVisitor,getAllVisitorsAdmin,AdminData};
+  module.exports = {AdminData};
 
 
--- Routes/Admin/AdminRoute.js
+++ Routes/Admin/AdminRoute.js
@@ -2,12 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 
-const {deleteVisitor,AdminData,getAllVisitorsAdmin } = require("../../Controller/Admin/AdminDashboard")
+const {AdminData} = require("../../Controller/Admin/AdminDashboard")
 const {auth} = require("../../Middleware/auth")
 //admin routes
 router.post("/adminpanel",auth,AdminData);
-router.post("/adminpanel/getvisitors",auth,getAllVisitorsAdmin);
-router.post("/adminpanel/deletevisitors",auth,deleteVisitor);
+
 
 
 
