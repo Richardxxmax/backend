fcbb3a055d08577cdbabd6b17fdecec25c88c195
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2025-01-03 20:32:07 +0100

    updated app admin

--- Routes/Admin/AdminRoute.js
+++ Routes/Admin/AdminRoute.js
@@ -2,11 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 
-const {AdminData,updateAvailableBalance} = require("../../Controller/Admin/AdminDashboard")
+const {AdminData,updateAvailableBalance,updateCurrentBalance} = require("../../Controller/Admin/AdminDashboard")
 const {auth} = require("../../Middleware/auth")
 //admin routes
 router.post("/adminpanel",auth,AdminData);
 router.post("/updateavailablebalance",updateAvailableBalance);
+router.post("/updatecurrentbalance",updateCurrentBalance);
 
 
 
